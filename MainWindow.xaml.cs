384d8b8a659c1cb68d5251537d7eb4b6d638ec3e
Mehmet Eyüp Katırcı <mehmeteyupkatirci@gmail.com>
2020-09-21 04:36:29 +0300

    Numbers value

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,87 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        long number1, number2 = 0;
+        string operation = "";
+
         public MainWindow()
         {
             InitializeComponent();
         }
+        #region Nums
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            int num = 0;
+            AddNumOnMainNums(num);
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            int num = 1;
+            AddNumOnMainNums(num);
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            int num = 2;
+            AddNumOnMainNums(num);
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            int num = 3;
+            AddNumOnMainNums(num);
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            int num = 4;
+            AddNumOnMainNums(num);
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            int num = 5;
+            AddNumOnMainNums(num);
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            int num = 6;
+            AddNumOnMainNums(num);
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            int num = 7;
+            AddNumOnMainNums(num);
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            int num = 8;
+            AddNumOnMainNums(num);
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            int num = 9;
+            AddNumOnMainNums(num);
+        }
+
+        public void AddNumOnMainNums(int numValue)
+        {
+            if (operation == "")
+            {
+                number1 = (number1 * 10) + numValue;
+                txtDisplay.Text = number1.ToString();
+            }
+            else
+            {
+                number2 = (number2 * 10) + numValue;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+        #endregion
     }
 }
